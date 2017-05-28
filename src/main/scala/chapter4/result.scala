9810284a415e9bc3f0103ef7e042959c11467961
ouchi2501 <unclesam2501@gmail.com>
2017-05-28 16:10:51 +0900

    capther4 exercise4.2 ~ 4.5

--- src/main/scala/chapter4/result.scala
+++ src/main/scala/chapter4/result.scala
@@ -5,8 +5,10 @@ package chapter4
   */
 object result {
   def main(args: Array[String]): Unit = {
-    val test = null
-    println(Option(test).getOrElse(0))
+    println(Option.variance(Seq(1, 2, 3)))
+    println(Option.sequence(List(Some(1), Some(2), Some(3))))
+    println(Option.sequence(List(Some(1), None, Some(3))))
+    println(Option.traverse(List("1", "2", "3"))(a => Some(a.toInt)))
   }
 }
 
@@ -25,8 +27,28 @@ sealed trait Option[+A] {
 
   def orElse[B >: A](ob: => Option[B]): Option[B] =
     this map (Some(_)) getOrElse ob
+
 }
 
 case class Some[+A](get: A) extends Option[A]
 
 case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] = if (xs.isEmpty) None else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] = mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    a flatMap (a2 => b map (b2 => f(a2, b2)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h.flatMap(h2 => sequence(t).map(h2 :: _))
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => map2(f(h), traverse(t)(f))(_ :: _)
+  }
+}
